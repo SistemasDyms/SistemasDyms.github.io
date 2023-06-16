67bc2574e082e4b2af480e1c0661a39a3ad7c430
Dairon Moreno <dairon9gmail.com>
2023-06-15 22:26:48 -0500

    cambios en el footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,32 @@
- $(document).ready(function()
-{
+$(document).ready(function () {
   var contador = 1;
 
-  $("#btn-menu").click(function()
-  {
-      if(contador == 1){
+  $("#btn-menu").click(function () {
+    if (contador == 1) {
       $("#menu").animate({
-        left:'0'
+        left: "0",
       });
       contador = 0;
-      }else{
-        contador = 1;
+    } else {
+      contador = 1;
       $("#menu").animate({
-        left:'-100%'
-      });	
+        left: "-100%",
+      });
     }
-});
+  });
 });
 
-const typed = new Typed('.typed',{
-  strings : ['Entusiasta','Autodidacta','Comprometido'], 
-  smartBackspace : true,
-  typeSpeed : 100,
-  backSpeed : 100 , 
+const typed = new Typed(".typed", {
+  strings: ["Entusiasta", "Autodidacta", "Comprometido"],
+  smartBackspace: true,
+  typeSpeed: 100,
+  backSpeed: 100,
   loop: true,
-  cursorChar: ' |',
-  contentType : 'html'
+  cursorChar: " |",
+  contentType: "html",
 });
+
+let element = document.querySelector("h4");
+let li = document.links[3];
+let form = document.forms;
+console.log(form);
